ab04bdb3ed75e278ad831f68511d90cc2316ff95
unknown <emmco96@gmail.com>
2025-02-14 04:18:59 +0100

    new

--- script.js
+++ script.js
@@ -159,4 +159,20 @@ function toggleFAQ(id) {
     });
   }
 
-  
+  function closeNotification() {
+    const notificationBar = document.getElementById("notificationBar");
+    if (notificationBar) {
+        notificationBar.style.right = "-100%"; // Slide out to the right
+        setTimeout(() => {
+            notificationBar.style.display = "none";
+        }, 500);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const notificationBar = document.getElementById("notificationBar");
+    if (notificationBar) {
+        notificationBar.style.display = "block";
+        setTimeout(closeNotification, 5000);
+    }
+});
